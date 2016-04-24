97e096fc349513c16ff08d5186697a2f2026758e
Daryl Eisner <ucsdmath.project@gmail.com>
2016-04-24 13:35:10 -0700

    Update

--- src/CronScheduler/AbstractCronScheduler.php
+++ src/CronScheduler/AbstractCronScheduler.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the UCSDMath package.
  *
- * (c) UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
+ * Copyright 2016 UCSD Mathematics | Math Computing Support <mathhelp@math.ucsd.edu>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
